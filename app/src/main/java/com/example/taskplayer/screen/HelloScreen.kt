061e19c43a9f4cbaf757b5d2a1543f1a3566208d
DanilaSchetinkin <danya.sch2015@gmail.com>
2025-07-10 17:36:55 +0300

    Welcome

--- app/src/main/java/com/example/taskplayer/screen/HelloScreen.kt
+++ app/src/main/java/com/example/taskplayer/screen/HelloScreen.kt
@@ -2,10 +2,15 @@ package com.example.taskplayer.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,7 +25,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.example.taskplayer.R
 import com.example.taskplayer.ui.theme.LightGreen
@@ -29,30 +37,66 @@ import com.example.taskplayer.ui.theme.mediafontFamily
 
 @Composable
 fun HelloScreen(){
-
-    Image(painter = painterResource(R.drawable.forest_les),
-        contentDescription = null,
-        Modifier.fillMaxSize(),
-        contentScale = ContentScale.Crop)
-
-
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center)
-    {
-        Image(painter = painterResource(R.drawable.logo),
+        Image(
+            painter = painterResource(R.drawable.forest_les),
             contentDescription = null,
-            modifier = Modifier.size(height = 199.dp, width = 191.dp))
+            Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
 
-            Text(text = "ПРИВЕТ", style = MediaTheme.typography.alegreyaBoldStart)
-            Text(text ="Наслаждайся отборочными.", style = MediaTheme.typography.alegreyaSans20)
-            Text(text = "Будь внимателен.", style = MediaTheme.typography.alegreyaSans20)
-            Text(text = "Делай хорошо.", style = MediaTheme.typography.alegreyaSans20)
 
-        Button(onClick = {}, colors =  ButtonDefaults.buttonColors(containerColor = LightGreen), shape = RoundedCornerShape(10.dp) ) {
-            Text(text = "Войти в аккаунт", style = MediaTheme.typography.alegreyaSans25)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        )
+        {
+            Spacer(modifier = Modifier.height(130.dp))
+
+            Box(modifier = Modifier.size(width = 191.dp, height = 199.dp)) {
+                Icon(
+
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = null,
+
+
+                    )
+
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = "ПРИВЕТ", style = MediaTheme.typography.alegreyaBoldStart
+                )
+                Text(
+                    text = "Наслаждайся отборочными.",
+                    style = MediaTheme.typography.alegreyaSans20
+                )
+                Text(text = "Будь внимателен.", style = MediaTheme.typography.alegreyaSans20)
+                Text(text = "Делай хорошо.", style = MediaTheme.typography.alegreyaSans20)
+
+            }
+
+            Spacer(modifier = Modifier.height(120.dp) )
+                Button(
+                    onClick = {},
+                    modifier = Modifier.width(321.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = LightGreen),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(text = "Войти в аккаунт", style = MediaTheme.typography.alegreyaSans25)
+                }
+
+                Row(modifier = Modifier.padding(top = 20.dp)) {
+                    Text("Ещё нет аккаунта?", style = MediaTheme.typography.alegreyaSans20400)
+                    Text("Зарегестрируйтесь", style = MediaTheme.typography.alegreyaSans20)
+                }
+
         }
-    }
+
 }
 
 
